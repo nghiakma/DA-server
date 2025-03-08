c20208fa8c48a26f720fb3a87cf2eda6ae8da592
nghiakma <lam47788@gmail.com>
2025-03-08 19:09:57 +0700

    registrationUser

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,3 +1,4 @@
+import { Message } from './../node_modules/chromium-bidi/lib/cjs/protocol/generated/webdriver-bidi.d';
 import dotenv from "dotenv";
 dotenv.config();
 import { Request, Response, NextFunction } from "express";
@@ -6,8 +7,51 @@ import ErrorHandler from "../utils/ErrorHandler";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ejs from "ejs";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+import path from "path";
+import { sendMail } from "../utils/sendMail";
 
 
+export const registrationUser = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+     try {
+        const {email} = req.body;
+        
+        const isEmailExist = await userModel.findOne({email});
+  
+        if(isEmailExist){
+          return next(new ErrorHandler("Email đã tồn tại",400));
+        }
+        
+        const newUser = new userModel(req.body);
+
+        const {token, activationCode} = createActivationToken(newUser);
+
+   
+
+        const data = {user: {name:newUser.name}, activationCode}
+
+        try {
+            await sendMail({
+                email: newUser.email,
+                subject: "Kích hoạt tài khoản của bạn",
+                template: "activation-mail.ejs",
+                data,
+              });
+      
+              return res.status(201).json({
+                success: true,
+                message: `Vui lòng kiểm tra tài khoản của bạn: ${newUser.email} để kích hoạt tài khoản!`,
+                activationToken: token,
+              });
+        } catch (error: any) {
+            return next(new ErrorHandler(error.message,400));
+        }
+
+     } catch (error: any) {
+        return next(new ErrorHandler(error.message,400));
+     }
+    }
+);
 
 interface IActivationToken {
   token: string;
