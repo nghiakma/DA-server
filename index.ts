81834d20795dae61c328cfd6a755058364f25759
nghiakma <lam47788@gmail.com>
2025-03-08 12:40:16 +0700

    connectDB

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import http from "http";
 import { app } from "./app";
 import dotenv from "dotenv";
+import connectDB from "./utils/db";
 dotenv.config();
 
 const server = http.createServer(app);
 
 server.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
+    connectDB();
 });
