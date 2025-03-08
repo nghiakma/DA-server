bd4681d6999810c847e4ac4f852ac0a30b6d42b2
nghiakma <lam47788@gmail.com>
2025-03-08 11:50:19 +0700

    Test & Cors

--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { NextFunction,Request,Response } from "express";
 export const app = express();
 import cors from "cors";
 import { rateLimit } from "express-rate-limit";
@@ -18,5 +18,26 @@ const limiter = rateLimit(
 app.use(express.json({limit: "100mb"}));
 app.use(express.static('uploads'));
 app.use(cookieParser());
-
 app.use(limiter);
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    credentials: true,
+    allowedHeaders: '*'
+}));
+app.options('*', cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+}));
+class AppError extends Error {
+    statusCode: number;
+    constructor(message: string, statusCode: number) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new AppError(`Đường dẫn ${req.originalUrl} không tìm thấy`, 404);
+    next(err);
+})
