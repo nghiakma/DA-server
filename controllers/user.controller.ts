e40dbce5f60029546ce81ec7c2628ad4bc7e4f65
nghiakma <lam47788@gmail.com>
2025-03-08 18:35:20 +0700

    throwActivationSecret

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -7,24 +7,30 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ejs from "ejs";
 import jwt, { JwtPayload, Secret } from "jsonwebtoken";
 
+
+
 interface IActivationToken {
-    token: string;
-    activationCode: string;
+  token: string;
+  activationCode: string;
 }
 
-export const createActivationToken = (user: any): IActivationToken => {
-    const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
-  
-    const token = jwt.sign(
-      {
-        user,
-        activationCode,
-      },
-      process.env.ACTIVATION_SECRET as Secret,
-      {
-        expiresIn: "5m",
-      }
-    );
-  
-    return { token, activationCode };
-  };
+export const createActivationToken = (user: IUser): IActivationToken => {
+  const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+
+  if (!process.env.ACTIVATION_SECRET) {
+    throw new Error("ACTIVATION_SECRET is not defined in environment variables");
+  }
+
+  const token = jwt.sign(
+    {
+      user,
+      activationCode,
+    },
+    process.env.ACTIVATION_SECRET as Secret,
+    {
+      expiresIn: "5m",
+    }
+  );
+
+  return { token, activationCode };
+};
