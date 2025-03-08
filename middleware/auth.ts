d1a72da05fd6883461ae17505486643ab9c6add1
nghiakma <lam47788@gmail.com>
2025-03-08 21:55:49 +0700

    authenticated

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,21 @@
 import { NextFunction, Request, Response } from "express"
 import ErrorHandler from "../utils/ErrorHandler"
+import { CatchAsyncError } from "./catchAsyncErrors";
+import jwt,{ JwtPayload } from "jsonwebtoken";
 
-
+export const isAuthenticated = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const access_token = req.cookies.accessToken;
+        if(!access_token){
+            return next(new ErrorHandler("Vui lòng đăng nhập để truy cập tài nguyên này",401));
+        }
+        const decoded = jwt.decode(access_token) as JwtPayload;
+        if (!decoded) {
+            return next(new ErrorHandler("access token không hợp lệ", 400));
+          }
+          next();
+    }
+)
 
 /**
  * Middleware để ủy quyền vai trò người dùng.
