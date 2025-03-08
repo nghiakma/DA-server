01020fd298815e5d5fa24b5dc99623eeeff16731
nghiakma <lam47788@gmail.com>
2025-03-08 14:55:52 +0700

    activeCode

---
+++ controllers/user.controller.ts
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Request, Response, NextFunction } from "express";
+import userModel, { IUser } from "../models/user.model";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import ejs from "ejs";
+import jwt, { JwtPayload, Secret } from "jsonwebtoken";
+
+interface IActivationToken {
+    token: string;
+    activationCode: string;
+}
+
+export const createActivationToken = (user: any): IActivationToken => {
+    const activationCode = Math.floor(1000 + Math.random() * 9000).toString();
+  
+    const token = jwt.sign(
+      {
+        user,
+        activationCode,
+      },
+      process.env.ACTIVATION_SECRET as Secret,
+      {
+        expiresIn: "5m",
+      }
+    );
+  
+    return { token, activationCode };
+  };
