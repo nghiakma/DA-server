ba4e1d70217c04dd8fb4074b6eaa9cd507fbccad
nghiakma <lam47788@gmail.com>
2025-03-09 08:34:49 +0700

    createLayout

---
+++ controllers/layout.controller.ts
@@ -0,0 +1,52 @@
+import { Request, Response, NextFunction } from "express";
+import ErrorHandler from "../utils/ErrorHandler";
+import { CatchAsyncError } from "../middleware/catchAsyncErrors";
+import LayoutModel from "../models/layout.model";
+
+
+// create layout
+export const createLayout = CatchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { type } = req.body;
+      const isTypeExist = await LayoutModel.findOne({ type });
+      if (isTypeExist) {
+        return next(new ErrorHandler(`${type} already exist`, 400));
+      }
+      if (type === "FAQ") {
+        const { faq } = req.body;
+        //dùng promise all chạy 3 tác vụ song song trả về kết qua khi tất cả chúng hoàn thành
+        const faqItems = await Promise.all(
+          faq.map(async (item: any) => {
+            return {
+              question: item.question,
+              answer: item.answer,
+            };
+          })
+        );
+        await LayoutModel.create({ type: "FAQ", faq: faqItems });
+      }
+      if (type === "Categories") {
+        const { categories } = req.body;
+        const categoriesItems = await Promise.all(
+          categories.map(async (item: any) => {
+            return {
+              title: item.title,
+            };
+          })
+        );
+        await LayoutModel.create({
+          type: "Categories",
+          categories: categoriesItems,
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        message: "Layout đã được tạo thành công",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
