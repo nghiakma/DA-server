e00217f29dc208908cd523b92f246cd5568d7e48
nghiakma <lam47788@gmail.com>
2025-03-09 07:18:21 +0700

    notiModel

---
+++ models/notification.model.ts
@@ -0,0 +1,37 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+
+export interface INotification extends Document {
+  title: string;
+  message: string;
+  status: string;
+  userId: mongoose.Schema.Types.ObjectId;
+}
+
+const notificationSchema = new Schema<INotification>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "unread",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification: Model<INotification> = mongoose.model("Notification", notificationSchema);
+
+export default Notification;
