d2c6aea08c68e2d76d9be07438965d350a4d4925
nghiakma <lam47788@gmail.com>
2025-03-09 09:50:16 +0700

    getLayout

--- routes/layout.route.ts
+++ routes/layout.route.ts
@@ -1,6 +1,11 @@
 import express from "express";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
-import { createLayout } from "../controllers/layout.controller";
+import { createLayout, editLayout, getLayoutByType } from "../controllers/layout.controller";
 export const layoutRouter = express.Router();
 
 layoutRouter.post("/create-layout", isAutheticated,authorizeRoles("admin"), createLayout);
+
+layoutRouter.put("/edit-layout", isAutheticated,authorizeRoles("admin"), editLayout);
+
+
+layoutRouter.get("/get-layout/:type",getLayoutByType);
