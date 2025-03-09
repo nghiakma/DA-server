27edf56ce7bf40b3dec8fecb13863319f0d49ef3
nghiakma <lam47788@gmail.com>
2025-03-09 16:00:00 +0700

    deleteWishCourse

--- routes/wishlist.route.ts
+++ routes/wishlist.route.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import {
     addWishCourse,
+    deleteWishCourseFromWishListOfUser,
     fetchWishListOfUser,
 } from "../controllers/wishlist.controller";
 import {
@@ -12,5 +13,7 @@ export const wishListRouter = express.Router();
 wishListRouter.post('/wishlist', isAutheticated, addWishCourse);
 
 wishListRouter.get('/wishlist', isAutheticated, fetchWishListOfUser);
+
+wishListRouter.delete('/wishlist', isAutheticated, deleteWishCourseFromWishListOfUser);
 
 
