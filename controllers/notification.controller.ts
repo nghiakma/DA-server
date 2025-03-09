5b2a6a96b23eaecd4de1cbd6c82aa1345c057984
nghiakma <lam47788@gmail.com>
2025-03-09 07:54:35 +0700

    use cron delete notification in 30 day

--- controllers/notification.controller.ts
+++ controllers/notification.controller.ts
@@ -48,3 +48,9 @@ export const updateNotification = CatchAsyncError(
       }
     }
   );
+
+cron.schedule("0 0 0 * * *", async() => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    await notificationModel.deleteMany({status:"read",createdAt: {$lt: thirtyDaysAgo}});
+    console.log('Đã xóa thông báo đã đọc');
+});
